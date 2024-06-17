86f6a545a60337989f8316fd9eddcec7b50cc389
DF-dev-rep <benjiproper@gmail.com>
2024-06-17 08:29:29 +0100

    test

--- scripts/configure_network.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Configuring network..."
-sleep 2  # Simulate some work
-echo "Network configuration complete."
--- utils/validate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-validate_root() {
-    if [ "$(id -u)" -ne 0 ]; then
-        log_error "This script must be run as root. Use sudo."
-        exit 1
-    fi
-}
-
